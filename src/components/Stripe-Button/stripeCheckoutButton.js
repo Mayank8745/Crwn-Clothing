fbbb42da4ca83debd5d6f324ca9eb860f4bad08f
Mayank Rajput <65065059+Mayank8745@users.noreply.github.com>
2023-02-25 17:34:45 +0530

    Stripe is Updated

--- src/components/Stripe-Button/stripeCheckoutButton.js
+++ src/components/Stripe-Button/stripeCheckoutButton.js
@@ -3,7 +3,7 @@ import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = ; // API OF STRIPE
+  const publishableKey = "pk_test_51KTtJsEcx25mceB9CNDthKvmVvRKy8yXDRZCpY089yvrU5qK1IScNT0WEV1cWMc91tbLcHedUGyrsqtJNEajUciB001q9I6odp"; // API OF STRIPE
   const onToken = (token) => {
     console.log(token);
     alert("Payement Successful");
